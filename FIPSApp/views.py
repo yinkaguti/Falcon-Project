bdc37824455c8c620541b8a8477a825e75819004
Olayinka Oloko <olayinka2good@gmail.com>
2019-11-12 13:34:39 +0100

    DJANGO PROJECT, FIPSApp

--- FIPSApp/views.py
+++ FIPSApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 
 # Create your views here.
 
@@ -12,4 +13,8 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post,pk=pk)
     return render(request,'FIPSApp/post_detail.html',{'post':post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'FIPSApp/post_edit.html',{'form':form})
     
